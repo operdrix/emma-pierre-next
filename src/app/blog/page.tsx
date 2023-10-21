4e5b833d03b42e47d857e83539b4d55c2205c825
Olivier PERDRIX <olivierperdrix@live.fr>
2023-10-21 10:00:33 +0200

    * feat(blog): add new page module and component for blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './page.module.scss'
+
+const Blog = () => {
+  return (
+    <div className={styles.container}>Blog</div>
+  )
+}
+
+export default Blog
